c1dbeb2be681f9198842dde7f320606ebb6490f0
Krzysztof <kmisiejuk15@gmail.com>
2024-04-12 19:07:49 +0200

    news filter front

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,21 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const News = require('../models/news');
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.render('news', { title: 'News' })
-})
+router.get('/', async (req, res) => {
 
-module.exports = router
+
+
+  try {
+    const search = req.query.search || " "
+    const data = await News.find({title: new RegExp(search.trim(), "i")}).sort({created: 1})
+  
+    res.render('news', { title: 'News', data, search });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Wystąpił błąd podczas pobierania danych.');
+  }
+});
+
+module.exports = router;
